49a4b15147e7dea54e748c7167199b6f39bbade4
Richard Walter <richardwalter515@gmail.com>
2021-01-29 10:55:32 -0500

    updated app file with routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Search from "./components/Search"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={About} />
+        <Route exact path="/discover" component={Discover} />
+        <Route path="/search" component={Search} />
+      </div>
+    </Router>
   );
 }
 
